1fd998e0156c2a6ea3a8b0ebcb7e47767ec4ce57
karthick111 <karthickrko61@gmail.com>
2017-08-06 18:46:08 +0200

    Revert "fs: sdcardfs: Import lenovo sdcardfs from oos zip"
    This reverts commit 5ca9ca1a554213e3046c746d57168e9973e4102e.

--- fs/sdcardfs/multiuser.h
+++ fs/sdcardfs/multiuser.h
@@ -1,35 +1,36 @@
 /*
  * fs/sdcardfs/multiuser.h
  *
- * Copyright (c) 2015 Lenovo Co. Ltd
- *   Authors: liaohs , jixj
+ * Copyright (c) 2013 Samsung Electronics Co. Ltd
+ *   Authors: Daeho Jeong, Woojoong Lee, Seunghwan Hyun,
+ *               Sunghwan Yun, Sungjong Seo
  *
  * This program has been developed as a stackable file system based on
- * the WrapFS which written by 
+ * the WrapFS which written by
  *
- * Copyright (c) 1998-2014 Erez Zadok
- * Copyright (c) 2009	   Shrikar Archak
- * Copyright (c) 2003-2014 Stony Brook University
- * Copyright (c) 2003-2014 The Research Foundation of SUNY
+ * Copyright (c) 1998-2011 Erez Zadok
+ * Copyright (c) 2009     Shrikar Archak
+ * Copyright (c) 2003-2011 Stony Brook University
+ * Copyright (c) 2003-2011 The Research Foundation of SUNY
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
+ * This file is dual licensed.  It may be redistributed and/or modified
+ * under the terms of the Apache 2.0 License OR version 2 of the GNU
+ * General Public License.
  */
 
 #define MULTIUSER_APP_PER_USER_RANGE 100000
 
 typedef uid_t userid_t;
-typedef long appid_t;
+typedef uid_t appid_t;
 
 static inline userid_t multiuser_get_user_id(uid_t uid) {
     return uid / MULTIUSER_APP_PER_USER_RANGE;
-}       
-        
+}
+
 static inline appid_t multiuser_get_app_id(uid_t uid) {
     return uid % MULTIUSER_APP_PER_USER_RANGE;
-}       
-    
+}
+
 static inline uid_t multiuser_get_uid(userid_t userId, appid_t appId) {
     return userId * MULTIUSER_APP_PER_USER_RANGE + (appId % MULTIUSER_APP_PER_USER_RANGE);
 }
