dfaac328e9f9084b7de0f9381244da9419e5a775
karthick111 <karthickrko61@gmail.com>
2017-11-03 14:05:23 +0100

    drivers: cleanup wingtech codes

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -26,8 +26,6 @@
 #include "mdss_dsi.h"
 #include "mdss_dba_utils.h"
 
-#include <linux/hardware_info.h>
-
 #define DT_CMD_HDR 6
 #define MIN_REFRESH_RATE 48
 #define DEFAULT_MDP_TRANSFER_TIME 14000
@@ -2734,7 +2732,6 @@ error:
 	return -EINVAL;
 }
 
-extern char Lcm_name[HARDWARE_MAX_ITEM_LENGTH];
 int mdss_dsi_panel_init(struct device_node *node,
 	struct mdss_dsi_ctrl_pdata *ctrl_pdata,
 	int ndx)
@@ -2759,7 +2756,6 @@ int mdss_dsi_panel_init(struct device_node *node,
 	} else {
 		pr_info("%s: Panel Name = %s\n", __func__, panel_name);
 		strlcpy(&pinfo->panel_name[0], panel_name, MDSS_MAX_PANEL_LEN);
-	        strcpy(Lcm_name,panel_name);
 	}
 	rc = mdss_panel_parse_dt(node, ctrl_pdata);
 	if (rc) {
